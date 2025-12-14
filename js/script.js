a1af56489fa1119dcaae24f1d738d08edadff4ca
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2025-12-15 00:22:12 +0530

    Add DOM element references for controls

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 let speech = new SpeechSynthesisUtterance();
 let voices = [];
+
+// Elements
+const textInput = document.getElementById("textinput");
+const speakBtn = document.getElementById("speakBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resumeBtn = document.getElementById("resumeBtn");
+const voiceSelect = document.getElementById("voiceSelect");
