834db147047b9d8f17fb275f9bd5c14c3d669f28
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2025-12-15 00:22:40 +0530

    Implement voice loading and dropdown population

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ const speakBtn = document.getElementById("speakBtn");
 const stopBtn = document.getElementById("stopBtn");
 const resumeBtn = document.getElementById("resumeBtn");
 const voiceSelect = document.getElementById("voiceSelect");
+
+// Load voices
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+
+    voiceSelect.innerHTML = "";
+
+    voices.forEach((voice, i) => {
+        voiceSelect.options[i] = new Option(
+            voice.name + " (" + voice.lang + ")", i
+        );
+    });
+
+    if (voices.length > 0) {
+        speech.voice = voices[0];
+    }
+};
